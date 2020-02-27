8b4834b1cc472e28c2555241ec8c2546eeebefeb
kranz fontanilla <me@kranznikolai.com>
2020-02-27 14:25:44 -0500

    update

---
+++ SentimentAnalysis/SentimentAnalysis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SentimentAnalysis;
+using SentimentAnalysisML.Model;
+namespace SentimentAnalysis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var input = new ModelInput();
+      input.SentimentText = "harmony between people of this village";
+
+      ModelOutput results = ConsumeModel.Predict(input);
+      Console.WriteLine($"Text: {input.SentimentText}\n Toxic: {results.Prediction}");
+    }
+  }
+}
